232d2f709117864a408c8c08e34323156b15f986
Денис Филлер <denis.filler@yandex.ru>
2023-04-14 17:21:23 +0300

    +

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,32 +1,60 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=100, unique=True)
-    description = models.TextField()
+    title = models.CharField(
+        verbose_name='Группа',
+        help_text='Выберите группу для поста',
+        max_length=200
+    )
+    slug = models.SlugField(
+        verbose_name='Слаг группы',
+        unique=True
+    )
+    description = models.TextField(
+        verbose_name='Описание группы',
+        help_text='Введите описание группы'
+    )
 
-    def __str__(self) -> str:
+    class Meta:
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
+
+    def __str__(self):
         return self.title
 
 
 class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User,
-                               on_delete=models.CASCADE)
-    group = models.ForeignKey(Group,
-                              blank=True,
-                              null=True,
-                              on_delete=models.SET_NULL)
-
-    def __str__(self) -> str:
-        return self.text[0:15]
+    text = models.TextField(
+        verbose_name='Пост',
+        help_text='Введите текст поста',
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата',
+        auto_now_add=True
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        verbose_name='Автор'
+    )
+    group = models.ForeignKey(
+        Group,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='posts',
+        verbose_name='Группа',
+        help_text='Выберите группу',
+    )
 
     class Meta:
-        ordering = ['-pub_date']
-        default_related_name = 'posts'
+        ordering = ('-pub_date',)
+
+    def __str__(self):
+        return self.text[:15]
